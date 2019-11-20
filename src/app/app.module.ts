d54ab07e9b6897c4ebde65377b15cf3eb025567d
BrunoMarcelino <est.brunomarcelino@gmail.com>
2019-11-20 09:04:25 +0300

    root client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ComponentClientComponent } from './component-client/component-client.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -15,8 +16,9 @@ import { ComponentClientComponent } from './component-client/component-client.co
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule.forRoot([
-      { path: '', component: AppComponent },
+      { path: 'home', component: AppComponent },
       { path: 'client', component: ComponentClientComponent },
+      { path: '', redirectTo: '/home', pathMatch: 'full' }
     ])
   ],
   providers: [],
